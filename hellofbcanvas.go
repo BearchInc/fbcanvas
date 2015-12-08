86e017dd2a0931661ba103c6f06b3a7a82beedf3
Diego Borges <drborges.cic@gmail.com>
2015-12-08 11:11:51 -0200

    Create list cards

--- hellofbcanvas.go
+++ hellofbcanvas.go
@@ -1,16 +1,14 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
-	"html/template"
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
-	t, _ := template.ParseFiles("public/index.html")
-	data := struct{User string}{"Diego"}
-	t.Execute(w, data)
+	fmt.Fprint(w, "Hello World")
 }
 
 func init() {
-	http.HandleFunc("/app/", handler)
+	http.HandleFunc("/", handler)
 }
